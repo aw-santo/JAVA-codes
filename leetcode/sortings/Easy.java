34d1604afc6b65256785c80de77b312563c65951
sanket_ghormode <sanghormode2@gmail.com>
2021-12-29 10:55:04 +0530

    F:/Git/

--- leetcode/sortings/Easy.java
+++ leetcode/sortings/Easy.java
@@ -4,4 +4,27 @@ public class Easy {
     public static void main(String[] args){
         System.out.println("Hi this is san!");
     }
+
+//    1. https://leetcode.com/problems/merge-sorted-array/
+    static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int pt1 = 0;
+        int pt2 = 0;
+        while (pt1<m+n && pt2<n){
+            if (nums1[pt1]>=nums2[pt2]){
+                slide1(nums1, pt1);
+                nums1[pt1]=nums2[pt2];
+                pt1++;
+                pt2++;
+            }
+            else {
+                pt1++;
+            }
+        }
+    }
+    static void slide1(int[] arr, int index){
+        for (int i = arr.length-2;i >= index; i--) {
+            arr[i+1] = arr[i];
+        }
+    }
+
 }
