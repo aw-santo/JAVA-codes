f3edaaa38b08c4d7410e25390a4242de457cd319
sanket_ghormode <sanghormode2@gmail.com>
2022-01-01 10:41:39 +0530

    .

--- leetcode/sortings/Easy.java
+++ leetcode/sortings/Easy.java
@@ -1,6 +1,8 @@
 package com.san.leetcode.sortings;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Easy {
     public static void main(String[] args){
@@ -89,5 +91,26 @@ public class Easy {
                 return j;
         }
         return nums.length;
+    }
+
+//    2. https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> ls = new ArrayList<>();
+        int i = 0;
+        while(i<nums.length){
+            int correct_index = nums[i]-1;
+            if (nums[i] != nums[correct_index]){
+                int temp = nums[i];
+                nums[i] = nums[correct_index];
+                nums[correct_index] = temp;
+            }
+            else
+                i++;
+        }
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j+1)
+                ls.add(j+1);
+        }
+        return ls;
     }
 }
