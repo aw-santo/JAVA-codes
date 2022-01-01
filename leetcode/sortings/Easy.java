3e7b18bf553c093e02dfc394317c2cc43da7e087
sanket_ghormode <sanghormode2@gmail.com>
2022-01-01 10:24:36 +0530

    .

--- leetcode/sortings/Easy.java
+++ leetcode/sortings/Easy.java
@@ -70,8 +70,24 @@ public class Easy {
         }
     }
 
-//    1. https://leetcode.com/problems/merge-sorted-array/
-    static void merge(int[] nums1, int m, int[] nums2, int n) {
-
+//    1. https://leetcode.com/problems/missing-number/
+    public int missingNumber(int[] nums) {
+        int i = 0;
+        while(i<nums.length){
+            int correct_index = nums[i];
+            if (nums[i]<nums.length && nums[i]!=nums[correct_index]){
+                int temp = nums[i];
+                nums[i] = nums[correct_index];
+                nums[correct_index] = temp;
+            }
+            else {
+                i++;
+            }
+        }
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j]!=j)
+                return j;
+        }
+        return nums.length;
     }
 }
